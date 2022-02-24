4ce010c941ab930443f45ce27938203ba9ba2b70
Emmanuel Ponnudurai <eponnudurai@catchpoint.com>
2022-02-24 14:40:29 -0500

    dont want everyone to know its react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React, { CSSProperties } from 'react';
+import React, { CSSProperties } from "react";
 // import logo from './logo.svg';
-import logo from './yoga_me.png';
-import './App.css';
+import logo from "./yoga_me.png";
+import "./App.css";
 
 function App() {
   const mainTitleStyle: CSSProperties = {
-    paddingTop: '50px'
+    paddingTop: "50px",
   };
 
   return (
@@ -16,30 +16,52 @@ function App() {
           <code>I'm re-building my site</code>
         </p>
         <p>
-          <code>Meanwhile, pls feel free to contact me on any of the following places</code>
+          <code>
+            Meanwhile, pls feel free to contact me on any of the following
+            places
+          </code>
         </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
         <div>
-          <a className="App-link" target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/c/EmmanuelPonnudurai">YouTube</a>
+          <a
+            className="App-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.youtube.com/c/EmmanuelPonnudurai"
+          >
+            YouTube
+          </a>
         </div>
         <div>
-          <a className="App-link" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/emmanuelponnudurai/">Instagram</a>
+          <a
+            className="App-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.instagram.com/emmanuelponnudurai/"
+          >
+            Instagram
+          </a>
         </div>
         <div>
-          <a className="App-link" target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/emmanuel.ponnudurai/">Facebook</a>
+          <a
+            className="App-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/emmanuel.ponnudurai/"
+          >
+            Facebook
+          </a>
         </div>
         <div>
-          <a className="App-link" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/emmanuelponnudurai">LinkedIn</a>
+          <a
+            className="App-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/emmanuelponnudurai"
+          >
+            LinkedIn
+          </a>
         </div>
       </header>
-
     </div>
   );
 }
